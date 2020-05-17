50d9c41fe71326a47903cca659353bd8b7bcc32b
adelebrgx <adele.bourgeix@gmail.com>
2020-05-17 14:24:38 -0400

    todo button works

--- script.js
+++ script.js
@@ -5,14 +5,34 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+var item=0;
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  var tag = document.createElement("li");
-   var text = document.createTextNode("Go to the gym");
-   tag.appendChild(text);
-   var element = document.getElementById("todo-list");
-   element.appendChild(tag);
+    item+=1;
+    var tag = document.createElement("input");
+    tag.type="checkbox";
+    //tag.setAttribute(name, item);
+    
+    var label = document.createElement("input");
+    
+    var add=document.createElement("button");
+    //add.type="button";
+    add.innerHTML="Add";
+    //label.setAttribute(for, item);
+    //tag.appendChild(label);
+    
+    var br = document.createElement("br");
+    var li=document.createElement("li");
+    
+    var element = document.getElementById("todo-list");
+    
+    li.appendChild(tag);
+    li.appendChild(label);
+    li.appendChild(add);
+    li.appendChild(br);
+    
+    element.appendChild(li);
 }
