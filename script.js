64bdf1da3b504890ab3626b77f693fcc3d4a60d1
adelebrgx <adele.bourgeix@gmail.com>
2020-05-17 18:02:03 -0400

    delete works

--- script.js
+++ script.js
@@ -5,34 +5,66 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
-var item=0;
+
+var itemCount=0;
+var unchecked=0;
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-    item+=1;
+    itemCount+=1;
+    itemCountSpan.innerHTML=itemCount;
+    unchecked+=1;
+    uncheckedCountSpan.innerHTML=unchecked;
+    
     var tag = document.createElement("input");
     tag.type="checkbox";
-    //tag.setAttribute(name, item);
+    
+    tag.addEventListener( 'change', function() {
+    if(this.checked) {
+        unchecked-=1;
+        uncheckedCountSpan.innerHTML=unchecked;
+    } else {
+        unchecked+=1;
+        uncheckedCountSpan.innerHTML=unchecked;
+    }
+    });
     
     var label = document.createElement("input");
     
     var add=document.createElement("button");
-    //add.type="button";
-    add.innerHTML="Add";
-    //label.setAttribute(for, item);
-    //tag.appendChild(label);
+    add.innerHTML="Add Task";
+    add.addEventListener( 'click', function() {
+        console.log("been clicked add")
+    });
+   
     
     var br = document.createElement("br");
     var li=document.createElement("li");
+    li.id=itemCount;
+    
+    var del=document.createElement("button");
+    del.innerHTML="Delete Task";
+    del.addEventListener( 'click', function() {
+        
+        console.log(del.parentNode.id);
+        del.parentNode.remove();
+        itemCount-=1;
+        itemCountSpan.innerHTML=itemCount;
+        
+    });
     
     var element = document.getElementById("todo-list");
+    
     
     li.appendChild(tag);
     li.appendChild(label);
     li.appendChild(add);
+    li.appendChild(del);
     li.appendChild(br);
     
     element.appendChild(li);
+    
 }
+
